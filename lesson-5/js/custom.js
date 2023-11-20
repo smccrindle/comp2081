be63f0fa1746a7250259905e433ea6071429bb60
Scott McCrindle <scott@mccrindle.me>
2023-11-19 20:08:05 -0500

    Added completed lesson files obtained from K218

--- lesson-5/js/custom.js
+++ lesson-5/js/custom.js
@@ -1,16 +1,20 @@
 // Custom JavaScript
 // STEP 15: Call up the carousel using the id, and use the .carousel() method create the options object
 const carouselCars = document.getElementById("carousel-cars");
-const carouselCaption = document.querySelectorAll("figcaption");
 
-carouselCars.addEventListener('slide.bs.carousel', event => {
-  // do something...
-    carouselCars.classList.add("zoomit");
-});
-carouselCars.addEventListener('slid.bs.carousel', event => {
-    // do something...
-      carouselCars.classList.remove("zoomit");
-});
-  
+const carousel = new bootstrap.Carousel(carouselCars, {
+  interval: 3000,
+  pause: false,
+  touch: true,
+  wrap: true
+})
 // STEP 16: Change the interval to 5 seconds (set in milliseconds)
 // STEP 17: Try an event handler to zoom up the .carousel-caption after the slide completes sliding - by adding a class when the slide has finished animating, and removing it when it begins
+carouselCars.addEventListener('slid.bs.carousel', event => {
+    // add the .zoomit class for fun
+    carouselCars.classList.add("zoomit");
+}) 
+carouselCars.addEventListener('slide.bs.carousel', event => {
+    // remove the .zoomit class to get set up for the next zoomit!
+    carouselCars.classList.remove("zoomit");
+})
